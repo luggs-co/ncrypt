2a4129e2cbb0ef5748c9eecc4d1253be8d8b61af
Luggs <luggs-co@users.noreply.github.com>
2015-06-28 06:24:21 +0000

    changed file size function, previous cause weird calc scenarios

--- source/core.js
+++ source/core.js
@@ -110,17 +110,18 @@ $( function() {
 
 	function formatSizeUnits( bytes )
 	{
-		if ( ( bytes >> 30 ) & 0x3FF )
-			bytes = ( bytes >>> 30 ) + '.' + ( bytes & ( 3 * 0x3FF ) ) + ' GB' ;
-		else if ( ( bytes >> 20 ) & 0x3FF )
-			bytes = ( bytes >>> 20 ) + '.' + ( bytes & ( 2 * 0x3FF ) ) + ' MB' ;
-		else if ( ( bytes >> 10 ) & 0x3FF )
-			bytes = ( bytes >>> 10 ) + '.' + ( bytes & ( 0x3FF ) ) + ' KB' ;
-		else if ( ( bytes >> 1 ) & 0x3FF )
-			bytes = ( bytes >>> 1 ) + ' Bytes' ;
-		else
-			bytes = bytes + ' Byte' ;
-		return bytes ;
+		var thresh = 1024;
+		if(Math.abs(bytes) < thresh) {
+			return bytes + ' B';
+		}
+		var units = [ 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB' ];
+		var u = -1;
+		do {
+			bytes /= thresh;
+			++u;
+		} while( Math.abs( bytes ) >= thresh && u < units.length - 1 );
+
+		return bytes.toFixed( 2 ) + ' ' + units[u];
 	}
 
 	function formatPercent( current, total )
