53015a0a245a4b8d8f7630616a6c6cf931f7d48d
luggs-co <luggs-co@users.noreply.github.com>
2017-03-03 10:56:01 +0000

    bump version

--- www/inc/config.inc.php
+++ www/inc/config.inc.php
@@ -23,7 +23,7 @@
 		'site' => [
 			'url' => '/',
 			'name' => 'NCrypt',
-			'version' => 'v0.7.60',
+			'version' => 'v0.7.61',
 			'source' => 'https://github.com/luggs-co/ncrypt',
 			'contact' => 'mailto:contact@ncry.pt',
 			'attachments' => [
